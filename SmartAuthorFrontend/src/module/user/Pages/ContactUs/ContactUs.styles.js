e1c812b86f37ceae81aa59ecc9bd0328e5bd177b
Suraj <dev2@tekpyramid.com>
2025-02-05 16:11:21 +0530

    updates

--- SmartAuthorFrontend/src/module/user/Pages/ContactUs/ContactUs.styles.js
+++ SmartAuthorFrontend/src/module/user/Pages/ContactUs/ContactUs.styles.js
@@ -3,8 +3,8 @@ import theme from "../../../../theme/Themes";
 
 export const ContactUsContainer = styled.div`
   display: flex;
-  justify-content: center;
+  // justify-content: center;
   align-items: center;
-  min-height: 100vh;
+  // min-height: 100vh;
   // background: ${theme.colors.BackgroundGradient};
 `;
